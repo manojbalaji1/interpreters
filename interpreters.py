3bdef27082fe2e62d1c6d600b1615db68cb68558
Manoj Balaji J <manojbalaji1@gmail.com>
2020-01-11 16:48:05 +0530

    first commit

---
+++ interpreters.py
@@ -0,0 +1,47 @@
+class interPretr:
+    vertices = []
+    edges = [[], []]
+
+    def readApplications(self, inputfile):
+        with open(inputfile) as file:
+            pass
+        pass
+
+    def showAll(self):
+        print("--------Function showAll--------")
+        # find candidate count
+        candidates_count = 0
+        # find languages count
+        languages_count = 0
+
+        print("Total no. of candidates: {}".format(candidates_count))
+        print("Total no. of languages: {}".format(languages_count))
+
+        # print empty line
+        print("")
+
+        print("List of candidates:")
+        # code for listing candidates
+
+        print("List of languages:")
+        # code for listing languages
+
+        print("-----------------------------------------")
+        pass
+
+    def displayHireList(self):
+        """
+        This function displays the minimum number of candidates that need to be hired to
+        cover all the languages that are fed into the system.
+        :return:
+        """
+        pass
+
+    def displayCandidates(self, lang):
+        pass
+
+    def findDirectTranslator(self, langA, langB):
+        pass
+
+    def findTransRelation(self, langA, langB):
+        pass
